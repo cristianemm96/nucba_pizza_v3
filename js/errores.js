1523ea0096450ad4511892755708e40adef232c8
Cristian Mariño <cristian.emanuel.m96@gmail.com>
2022-06-30 15:58:12 -0300

    Nombre de variable contenedora cambiado

--- js/errores.js
+++ js/errores.js
@@ -1,20 +1,20 @@
 import { resultado } from "./interfaz.js";
 import {removerElementos} from './removerElems.js';
 
-const errorContainer = document.createElement("div");
+const contenedorError = document.createElement("div");
 const error = document.createElement("p");
-errorContainer.className = "error";
+contenedorError.className = "error";
 
 export const pizzaNoEncontrada = () => {
   removerElementos();
   error.innerHTML = "No se encontraron pizzas con ese nombre.";
-  errorContainer.appendChild(error);
-  resultado.append(errorContainer);
+  contenedorError.appendChild(error);
+  resultado.append(contenedorError);
 };
 
 export const pizzaVacia = () => {
   removerElementos();
   error.innerHTML = "El campo no puede estar vacio."
-  errorContainer.appendChild(error);
-  resultado.append(errorContainer);
+  contenedorError.appendChild(error);
+  resultado.append(contenedorError);
 };
